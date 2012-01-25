770d98ff28a25d76153752e6379d3a870f55dde9
bahia0019 <bahia0019@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-25 07:59:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/pin-it-on-pinterest/trunk@495064 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: Pin It On Pinterest
+Plugin URI: http://flauntyoursite.com/pin-it-on-pinterest
+Version: 0.9
+Author: Flaunt Your Site
+Description: Pin It On Pinterest places a Pin It button at the end of your posts, and allows you to predetermine what gets "Pinned" at Pinterest.
+Author URI: http://flauntyoursite.com
+License: GPL3
+*/
+
+$plugin_path=WP_PLUGIN_DIR.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__));
+$plugin_url=trailingslashit(plugins_url(basename(dirname(__FILE__))));
+
+include($plugin_path.'class-pinterest.php');
+
+?>
